b0cdf42990c1d7f0d265a5104cbaa803a64c417f
NickSchnepple <nschnepple@cnm.edu>
2019-04-20 16:36:44 -0600

    added set authorActivationToken

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -59,7 +59,7 @@ class Author {
 	 *
 	 * @param Uuid| string $newAuthorId value of new authorId
 	 * @throws \RangeException if $newAuthorId is not positive
-	 * @throws \TypeError if the authorId is not
+	 * @throws \TypeError if the authorId is not a string
 	 */
 
 	public function setAuthorId($newAuthorId): void {
@@ -73,4 +73,44 @@ class Author {
 		//convert and store the authorId
 		$this->profileId = $uuid;
 	}
+	/**
+	 * accessor methd for author activation token
+	 *
+	 * @return string value of the activation token
+	 */
+	public function getAuthorActivationToken() : ?string {
+		return ($this->authorActivationToken);
+	}
+	/**
+	 * mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken
+	 * @throws \InvalidArgumentException if the token is not a string or insecure
+	 * @throws \RangeException if the token is not exactly 32 characters
+	 * @throws \TypeError if the activation token is not a string
+	 */
+	public function setAuthorActivationToken(?string $newAuthorActivationToken): void {
+		if($newAuthorActivationToken === null) {
+			$this->authorActivationToken = null;
+			return;
+		}
+		$newAuthorActivationToken = strtolower(trim($newAuthorActivationToken));
+		if(ctype_xdigit($newAuthorActivationToken) === false) {
+			throw(new\RangeException("user activation is not valid"));
+		}
+		//make sure user activation token is only 32 characters
+		if(strlen($newAuthorActivationToken) !== 32) {
+			throw(new\RangeException("user activation token has to be 32"));
+		}
+		$this->authorActivationToken = $newAuthorActivationToken;
+	}
+	/**
+	 * accessor method for avatar url
+	 *
+	 * @param string $newAuthorAvatarUrl
+	 * @throws \InvalidArgumentException if $newAvatarUrl is not a string or insecure
+	 * @throws \RangeException if $newAvatarUrl is > 32 characters
+	 * @throws \TypeError if $newAvatarUrl is not a string
+	 */
+
 }
