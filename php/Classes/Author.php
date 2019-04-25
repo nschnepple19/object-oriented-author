a0f04d5f6d3a0c3e7433f55bce4f7d7658c5d6d1
NickSchnepple <nschnepple@cnm.edu>
2019-04-24 19:40:34 -0600

    fixed some errors with the getAuthorByAuthorId

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -318,8 +318,8 @@ class Author {
 	 * gets the author by authorId
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 * @param Uuid|string $tweetId tweet id to search for
-	 * @return Tweet|null Tweet found or null if not found
+	 * @param Uuid|string $authorId author id to search for
+	 * @return Author|null Tweet found or null if not found
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when a variable are not the correct data type
 	 */
